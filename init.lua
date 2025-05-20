4a135dec7fdfb302f2e4ab11484da391a9a311af
Kian Ryan <kian@orangetentacle.co.uk>
2025-05-20 15:36:50 +0100

    Add spelling

--- init.lua
+++ init.lua
@@ -34,7 +34,6 @@ opt.sidescrolloff = 8 -- Columns of context
 opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
 opt.smartcase = true -- Don't ignore case with capitals
 opt.smartindent = true -- Insert indents automatically
-opt.spelllang = { "en" }
 opt.splitbelow = true -- Put new windows below current
 opt.splitkeep = "screen"
 opt.splitright = true -- Put new windows right of current
@@ -58,8 +57,8 @@ opt.fillchars = {
   eob = " ",
 }
 
-vim.wo.colorcolumn = '80'
-
+opt.spell = true
+opt.spelllang = "en"
 
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
@@ -157,6 +156,7 @@ require("lazy").setup({
 {"williamboman/mason-lspconfig.nvim"},
 
 {"neovim/nvim-lspconfig"},
+{"nvimtools/none-ls.nvim"},
 { 'echasnovski/mini.nvim', version = false },
 {
   'catgoose/nvim-colorizer.lua',
